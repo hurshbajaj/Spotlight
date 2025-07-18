3f74073acc62881358a7c9bfd3b3d3507da6b3ff
Hursh Bajaj <hurshbajaj2011@gmail.com>
2025-07-18 06:15:22 +0530

    Options Page

--- index.js
+++ index.js
@@ -1,36 +1,6 @@
-function generateStarField() {
-    const totalStars = 100;
-
-    Array.from({ length: totalStars }).forEach(() => {
-        const sparkle = document.createElement('div');
-        sparkle.classList.add('star');
-
-        const diameter = 1 + Math.random() * 3;
-        Object.assign(sparkle.style, {
-            width: `${diameter}px`,
-            height: `${diameter}px`,
-            left: `${Math.random() * 100}vw`,
-            top: `${Math.random() * 100}vh`,
-            animationDuration: `${1 + Math.random() * 2}s`
-        });
-
-        document.body.appendChild(sparkle);
-    });
-}
-
+import {generateStarField, rise} from "./utils.js"
 generateStarField();
-
-function rise(images, min, max, totalDuration = 2000, maxDelay = 1500) {
-    images.forEach((img) => {
-        const delay = Math.random() * maxDelay;
-        const duration = Math.max(0, totalDuration - delay);
-
-        img.style.transition = `bottom ${duration}ms ease ${delay}ms`;
-
-        const randomMargin = Math.random() * (max - min) + min;
-        img.style.bottom = `${randomMargin}px`;
-    });
-}
+console.log("test");
 
 function drop_init(offset, totalDuration = 1000, maxDelay = 500) {
     const others = document.querySelectorAll("img, .spotlight, .dynamix");
---
+++ options.js
@@ -0,0 +1,31 @@
+import { generateStarField, rise } from './utils.js'
+
+generateStarField();
+
+window.onload = () => {
+    let buildings = document.querySelectorAll(".building_el");
+    rise(buildings, 0, -200, 1000, 300);
+
+    let spot = document.getElementById("spot");
+    spot.style.opacity = 1;
+
+    let auth_z = document.getElementById("auth_z");
+    let auth_o = document.getElementById("auth_o");
+    auth_z.style.opacity = 1;
+    auth_o.style.opacity = 1;
+
+    slideInFromLeft(document.querySelectorAll(".option"))
+};
+
+function slideInFromLeft(elements, totalDuration = 1500, maxDelay = 500) {
+    elements.forEach((el) => {
+        const delay = Math.random() * maxDelay;
+        const duration = Math.max(0, totalDuration - delay);
+
+        el.style.transition = `transform ${duration}ms ease`;
+
+        setTimeout(() => {
+            el.style.transform = `translateX(0)`;
+        }, delay);
+    });
+}
