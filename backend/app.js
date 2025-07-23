c073829051a125cf9abeaaa3e2e60bd40e01b211
Hursh Bajaj <hurshbajaj2011@gmail.com>
2025-07-24 00:02:59 +0530

    vercel

--- backend/app.js
+++ backend/app.js
@@ -1,54 +1,56 @@
 const express = require("express");
-const cp = require("cookie-parser");
+let cp = require("cookie-parser");
 const path = require("path");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const api_router = require("./api.js");
 
+const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log("MongoDB connection error:", err));
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("MongoDB connection error:", err));
 
-const app = express();
+let app = express();
+const PORT = 8080;
 
 app.use(cp());
 app.use(express.json());
 
-// Serve static folders
-app.use("/static", express.static(path.join(__dirname, "../static")));
-app.use("/public", express.static(path.join(__dirname, "../public"), {
-  extensions: ["html"]
-}));
-app.use("/user", is_auth, express.static(path.join(__dirname, "../prot_pub"), {
-  extensions: ["html"]
+app.use('/static', express.static(path.join(__dirname, "../static")));
+
+//protected routes, in ../prot_pub
+
+app.use("/user",is_auth ,express.static(path.join(__dirname, "../prot_pub"), {
+  extensions: ['html']
 }));
 
-// API routes
+app.use(express.static(path.join(__dirname, '../public'), {
+  extensions: ['html']
+}));
+
 app.use("/api", api_router);
 
-// Catch-all: redirect to root
 app.use((req, res) => {
   res.redirect("/");
 });
 
-// Auth middleware
+
 function is_auth(req, res, nxt) {
-  if ("is_auth" in req.cookies) {
-    nxt();
-  } else {
-    res.redirect("/menu");
-  }
+    if ("is_auth" in req.cookies) {
+        nxt()
+    }else{
+        res.redirect(`/menu`);
+    }
+
 }
 
+if(require.main === module){
+  app.listen(PORT, ()=>{
+      console.log(`Listening at PORT ${PORT}`);
+  })
+}
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Listening at PORT ${PORT}`);
-});
-
-
+module.exports = app;
